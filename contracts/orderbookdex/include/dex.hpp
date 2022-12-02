a90eb72c05fdc9c7dc8d9fd4ff486a1f40a7a657
Joss <281900360@qq.com>
2022-12-02 20:05:57 +0800

    update code style

--- contracts/orderbookdex/include/dex.hpp
+++ contracts/orderbookdex/include/dex.hpp
@@ -24,20 +24,21 @@ public:
         _global->save(get_self());
     }
 
-    [[eosio::action]] void init();
+    ACTION init();
     
-    [[eosio::action]] void setconfig(const dex::config &conf);
+    ACTION setconfig(const dex::config &conf);
 
-    [[eosio::action]] void setsympair(const extended_symbol &asset_symbol,
-                                      const extended_symbol &coin_symbol,
-                                      const asset &min_asset_quant, const asset &min_coin_quant,
-                                      bool only_accept_coin_fee, bool enabled);
+    ACTION setsympair(  const extended_symbol &asset_symbol,
+                        const extended_symbol &coin_symbol,
+                        const asset &min_asset_quant, const asset &min_coin_quant,
+                        bool only_accept_coin_fee, bool enabled);
 
-    [[eosio::action]] void onoffsympair(const uint64_t& sympair_id, const bool& on_off);
+    ACTION onoffsympair(const uint64_t& sympair_id, const bool& on_off);
 
-    [[eosio::on_notify("*::transfer")]] void ontransfer(const name& from, const name& to, const asset& quant, const string& memo);
+    [[eosio::on_notify("*::transfer")]] 
+    void ontransfer(const name& from, const name& to, const asset& quant, const string& memo);
 
-    [[eosio::action]] void withdraw(const name& user, const name &bank, const asset& quant, const string& memo);
+    ACTION withdraw(const name& user, const name &bank, const asset& quant, const string& memo);
 
     /**
      * new order, should deposit by transfer first
@@ -50,42 +51,41 @@ public:
      * @param external_id - external id, always set by application
      * @param order_config_ex - optional extended config, must authenticate by admin if set
      */
-    [[eosio::action]] void
-    neworder(const name &user, const uint64_t &sympair_id,
+    ACTION neworder(const name &user, const uint64_t &sympair_id,
             const name &order_side,
              const asset &limit_quant, const asset &frozen_quant,
              const asset &price, const uint64_t &external_id,
              const optional<dex::order_config_ex_t> &order_config_ex);
 
-    [[eosio::action]] void buy(const name &user, const uint64_t &sympair_id,
-                                    const asset &quantity, const asset &price, const uint64_t &external_id,
-                                    const optional<dex::order_config_ex_t> &order_config_ex);
+    ACTION buy( const name &user, const uint64_t &sympair_id,
+                const asset &quantity, const asset &price, const uint64_t &external_id,
+                const optional<dex::order_config_ex_t> &order_config_ex);
 
-    [[eosio::action]] void sell(const name &user, const uint64_t &sympair_id,
-                                     const asset &quantity, const asset &price,
-                                     const uint64_t &external_id,
-                                     const optional<dex::order_config_ex_t> &order_config_ex);
+    ACTION sell(const name &user, const uint64_t &sympair_id,
+                const asset &quantity, const asset &price,
+                const uint64_t &external_id,
+                const optional<dex::order_config_ex_t> &order_config_ex);
 
     /**
      *  @param max_count the max count of match item
      *  @param sym_pairs the symol pairs to match. is empty, match all
      */
-    [[eosio::action]] void match(const name &matcher, uint32_t max_count, const string &memo);
+    ACTION match(const name &matcher, uint32_t max_count, const string &memo);
 
-    [[eosio::action]] void cancel(const uint64_t& pair_id, const name& side, const uint64_t &order_id);
+    ACTION cancel(const uint64_t& pair_id, const name& side, const uint64_t &order_id);
 
-    [[eosio::action]] void cleandata(const uint64_t &max_count);
+    ACTION cleandata(const uint64_t &max_count);
 
-    [[eosio::action]] void name2uint(const name& n) { check(false, to_string(n.value)); };
+    ACTION name2uint(const name& n) { check(false, to_string(n.value)); };
 
-    [[eosio::action]] void openorderkey(const uint64_t sympair_id,
-                                        const name order_side,
-                                        const bool is_lower_bound);
+    ACTION openorderkey(const uint64_t sympair_id,
+                        const name order_side,
+                        const bool is_lower_bound);
 
     // using withdraw_action   = action_wrapper<"withdraw"_n, &dex_contract::withdraw>;
     using neworder_action   = action_wrapper<"neworder"_n, &dex_contract::neworder>;
-    using buy_action   = action_wrapper<"buy"_n, &dex_contract::buy>;
-    using sell_action  = action_wrapper<"sell"_n, &dex_contract::sell>;
+    using buy_action        = action_wrapper<"buy"_n, &dex_contract::buy>;
+    using sell_action       = action_wrapper<"sell"_n, &dex_contract::sell>;
     using match_action      = action_wrapper<"match"_n, &dex_contract::match>;
     using cancel_action     = action_wrapper<"cancel"_n, &dex_contract::cancel>;
 
@@ -96,23 +96,22 @@ public:
         uint8_t* c = (uint8_t*)d;
         for( uint32_t i = 0; i < s; ++i )
             (r += to_hex[(c[i]>>4)]) += to_hex[(c[i] &0x0f)];
-
         return r;
     }
 
-    std::string to_hex(const std::vector<char>& data ){
+    std::string to_hex( const std::vector<char>& data ){
        if( data.size() )
           return to_hex( data.data(), data.size() );
 
        return "";
     }
 
-    std::string to_hex(const std::array<uint8_t, 32> & bytes){
+    std::string to_hex( const std::array<uint8_t, 32> & bytes ){
         std::vector<char> chars(bytes.begin(), bytes.end());
         return to_hex(chars);
     } 
 
-    inline std::string str_to_upper(string_view str) {
+    inline std::string str_to_upper( string_view str ) {
         std::string ret(str.size(), 0);
         for (size_t i = 0; i < str.size(); i++) {
             ret[i] = std::toupper(str[i]);
@@ -120,7 +119,7 @@ public:
         return ret;
     }
 
-    inline uint64_t parse_uint64(string_view str) {
+    inline uint64_t parse_uint64( string_view str ) {
         safe<uint64_t> ret;
         to_int(str, ret);
         return ret.value;
@@ -128,8 +127,8 @@ public:
 
 private:
     dex::config get_default_config();
-    
-    void _allot_fee(const name &from_user, const name& bank, const asset& fee, const uint64_t order_id);
+
+    void _allot_fee( const name &from_user, const name& bank, const asset& fee, const uint64_t order_id );
 
     void match_sympair(const name &matcher, const dex::symbol_pair_t &sym_pair, uint32_t max_count,
                         uint32_t &matched_count, const string &memo);
